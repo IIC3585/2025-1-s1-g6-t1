90de6960813d35819270bb78a1d7ef7684088aee
Santiago Prado <santiagoprado2001@gmail.com>
2025-03-25 12:43:50 -0300

    avance spj

--- services.js
+++ services.js
@@ -24,6 +24,28 @@ const printCSVData = async (file) => {
   }
 }
 
+//2
+const rowstocolumns = async (file) => {
+    try {
+        const data = await readCSV(file);
+        const transposed = data[0].map((_, colIndex) => data.map(row => row[colIndex]));
+        console.log(transposed.map(row => row.join(',')).join('\n'));
+    } catch (error) {
+        console.error('Error processing CSV:', error);
+    }
+};
+
+//3
+const columnstorows = async (file) => {
+    try {
+        const data = await readCSV(file);
+        const transposed = data[0].map((_, colIndex) => data.map(row => row[colIndex]));
+        console.log(transposed.map(row => row.join(',')).join('\n'));
+    } catch (error) {
+        console.error('Error processing CSV:', error);
+    }
+};
+
 const insertrow = async (file, n, row) => {
     try {
         const data = await readCSV(file);
@@ -46,10 +68,16 @@ const insertcolumn = async (file, n, column) => {
     }
 }
 
+
+
 const newRow = ['Borja', 'Errazuriz', 'berrazuriz3@uc.cl']
 const newColumn = ["993456780", "995674543", "995674543", "995674543"]
 
 const file = 'data.csv';
 printCSVData(file);
+
+rowstocolumns(file);
+columnstorows(file);
+
 insertrow(file, 0, newRow);
 insertcolumn(file, 3, newColumn);
